82045570bbead0cea3e5542740e2c36373de353e
Bondarenko Ivan <i.bondarenko@loymax.ru>
2019-04-16 14:01:39 +0700

    Изменен скрипт и описание работы

--- make_task.ps1
+++ make_task.ps1
@@ -1,18 +1,23 @@
-# добавить параметр вызова имя ветки
-# создаватьт каталог с задачей. скачивать ветку
+п»ї# РґРѕР±Р°РІРёС‚СЊ РїР°СЂР°РјРµС‚СЂ РІС‹Р·РѕРІР° РёРјСЏ РІРµС‚РєРё
+# СЃРѕР·РґР°РІР°С‚СЊС‚ РєР°С‚Р°Р»РѕРі СЃ Р·Р°РґР°С‡РµР№. СЃРєР°С‡РёРІР°С‚СЊ РІРµС‚РєСѓ
 Param
 (
-    [Parameter (Mandatory=$true, HelpMessage="Введите название ветки репозитория Loymax")]
-    [string] $branchName,
-
-    [Parameter (Mandatory=$true, HelpMessage="Введите название ветки репозитория Integration")]
-	[string] $integrationBranchName,
     
-    [Parameter (Mandatory=$true, HelpMessage="Для сборки ARM введите любое значение")]    
-    [bool] $buildArm=$false,
+    [Parameter (Mandatory=$true, Position = 0)]
+    [string] $taskName,
 
-    [Parameter (Mandatory=$true, HelpMessage="Для сборки ARM плагинов введите любое значение")]
-    [bool] $buildPlugins=$false
+    [Parameter (Mandatory=$true, Position = 1)]
+    [string] $branchName = $taskName,
+
+    [Parameter (Mandatory=$true, Position = 2)]
+    [AllowEmptyString()]
+	[string] $integrationBranchName = "",
+    
+    [Parameter (Mandatory=$false, Position = 3)]    
+    [bool] $buildArm=$true,
+
+    [Parameter (Mandatory=$false, Position = 4)]
+    [bool] $buildPlugins=$true
 )
 
 $targetFolder = "C:\Users\i.bondarenko\source\tasks_tests2"
@@ -22,12 +27,19 @@ $currentLocation = Get-Location
 function Get-Repository-Branch {
 
     Param 
-    (    
+    (   
+        [string] $taskName, 
         [string] $branch, 
         [string] $repo,
         [bool] $build,
         [string] $nameOfArmFolder
     )
+
+    if([string]::IsNullOrWhiteSpace($branch)) {
+        $parts = ($repo -split "/")[-1]        
+        Write-Host "$parts skipped"
+        return $false;
+    }
 
     Write-Host "Branch Name: $branchName $repo $build"
 
@@ -35,23 +47,22 @@ function Get-Repository-Branch {
     $hasBranch = $checkBranch -like "*$branch*"
 
     if(!$hasBranch) {
-        Write-Host "Не найдена ветка $branchName" -ForegroundColor Red
-        return $false
+        Write-Host "РќРµ РЅР°Р№РґРµРЅР° РІРµС‚РєР° $branchName" -ForegroundColor Red
+        return $false;
     }
     	
-    $taskFolder = [System.IO.Path]::combine($targetFolder, $branch)
+    $taskFolder = [System.IO.Path]::combine($targetFolder, $taskName)
     if(!(Test-Path $taskFolder)){
-        # Write-Host "Папка $branchName уже создана" -ForegroundColor Red
+        # Write-Host "РџР°РїРєР° $branchName СѓР¶Рµ СЃРѕР·РґР°РЅР°" -ForegroundColor Red
         # return $false
         New-Item -ItemType directory -Path $taskFolder
     }
-    
-    Set-Location $taskFolder
+        
+    Set-Location $taskFolder    
 
     git clone -b $branch $repo    
 
-    Write-Host "Location: " -NoNewLine
-    Write-Host $currentLocation -ForegroundColor Green
+    Write-Host "Location: " (Get-Location) -ForegroundColor Green
    
     Set-Location $nameOfArmFolder
     Write-Host "Build $nameOfArmFolder..." -ForegroundColor Blue    
@@ -70,7 +81,7 @@ function Get-Repository-Branch {
 	}	    
     Write-Host "done" -ForegroundColor Blue
 
-    return $true;
+    return $true
 }
 
 function Get-Task(){
@@ -79,45 +90,46 @@ function Get-Task(){
 
         Write-Host "Get-Repository-Branch $branchName  https://tfs.loymax.net/DefaultCollection/_git/Loymax $buildArm"
       
-        $loymax = Get-Repository-Branch $branchName  "https://tfs.loymax.net/DefaultCollection/_git/Loymax" $buildArm "Loymax\ARMv2"
-        $integration = Get-Repository-Branch $integrationBranchName  "https://tfs.loymax.net/DefaultCollection/Loymax/_git/Integration" $buildPlugins  "Integration\Loymax.Arm.Plugins"
+        Get-Repository-Branch $taskName $branchName  "https://tfs.loymax.net/DefaultCollection/_git/Loymax" $buildArm "Loymax\ARMv2"
+        Get-Repository-Branch $taskName $integrationBranchName  "https://tfs.loymax.net/DefaultCollection/Loymax/_git/Integration" $buildPlugins  "Integration\Loymax.Arm.Plugins"
 
-        # Создаем сайт, если требуется
-        $siteName = $branchName
-        if($loymax) {
-            if (!(Test-Path IIS:\Sites\$siteName))
-            {        
-                Write-Host "creating $branchName.localhost web site ..." -ForegroundColor Yellow
-        
-                $physicalSitePath = [System.IO.Path]::combine($taskFolder, "Loymax\ARMv2\build")
-                $physicalAppPath = [System.IO.Path]::combine($taskFolder, "Loymax\Kernel\Loymax.SystemApi")
-        
-	            #if(!(Test-Path $physicalSitePath)){
-	            #	New-Item -ItemType directory -Path $physicalSitePath
-	            #}
-
-                Write-Host $physicalSitePath
-                Write-Host $physicalAppPath
-
-                $hostHeader = "$siteName.localhost"        
-                         
-                New-Website -Name $siteName -Port 80 -PhysicalPath $physicalSitePath -IPAddress "*" -HostHeader $hostHeader -ApplicationPool "DefaultAppPool"
-                New-WebApplication -Site $siteName -Name "api" -PhysicalPath $physicalAppPath -ApplicationPool "DefaultAppPool"
-
-                Write-Host "created" -ForegroundColor Green
-
-                Write-Host "Update hosts file..." -ForegroundColor Yellow -NoNewline       
-                ac -Encoding UTF8  C:\Windows\system32\drivers\etc\hosts "127.0.0.1    $hostHeader"
-
-                Write-Host "done" -ForegroundColor Green                        
-                        Write-Host ""
-                Write-Host "Try to open http://$hostHeader"
+        # РЎРѕР·РґР°РµРј СЃР°Р№С‚, РµСЃР»Рё С‚СЂРµР±СѓРµС‚СЃСЏ
+        $siteName = $taskName
+        $taskFolder = [System.IO.Path]::combine($targetFolder, $taskName)        
+        if (!(Test-Path IIS:\Sites\$siteName))
+        {                
+            Write-Host "creating $branchName.localhost web site ..." -ForegroundColor Yellow
+    
+            Write-Host "Task Folder $taskFolder" -ForegroundColor Green
+            $physicalSitePath = [System.IO.Path]::combine($taskFolder, "Loymax\ARMv2\build")
+            $physicalAppPath = [System.IO.Path]::combine($taskFolder, "Loymax\Kernel\Loymax.SystemApi")
+    
+            if(!(Test-Path $physicalSitePath)){
+            	New-Item -ItemType directory -Path $physicalSitePath
             }
-            else # Сайт уже существует, ничего не делаем
-            {
-                Write-Host "exist" -ForegroundColor Green
-            }
-        }       
+
+            Write-Host $physicalSitePath
+            Write-Host $physicalAppPath
+
+            $hostHeader = "$siteName.localhost"        
+                        
+            New-Website -Name $siteName -Port 80 -PhysicalPath $physicalSitePath -IPAddress "*" -HostHeader $hostHeader -ApplicationPool "DefaultAppPool"
+            New-WebApplication -Site $siteName -Name "api" -PhysicalPath $physicalAppPath -ApplicationPool "DefaultAppPool"
+
+            Write-Host "created" -ForegroundColor Green
+
+            Write-Host "Update hosts file..." -ForegroundColor Yellow -NoNewline       
+            Add-Content -Encoding UTF8  C:\Windows\system32\drivers\etc\hosts "127.0.0.1    $hostHeader"
+
+            Write-Host "done" -ForegroundColor Green                        
+                    Write-Host ""
+            Write-Host "Try to open http://$hostHeader"
+        }
+        else # РЎР°Р№С‚ СѓР¶Рµ СЃСѓС‰РµСЃС‚РІСѓРµС‚, РЅРёС‡РµРіРѕ РЅРµ РґРµР»Р°РµРј
+        {
+            Write-Host "exist" -ForegroundColor Green
+        }
+               
     }
     finally
     {
